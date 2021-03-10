5c6c82a8f19361119ddf6ae9c1f0a68670ec7419
ashrestha1 <oscaaah@gmail.com>
2021-03-10 23:22:18 +0800

    updated login screen

--- App.js
+++ App.js
@@ -1,36 +1,73 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import HomeScreen from './screens/HomeScreen'
-import PracticeHomeScreen from './screens/PracticeHomeScreen'
-import PracticeSetHomeScreen from './screens/PracticeSetHomeScreen'
-import PracticeSetDetailsScreen from './screens/PracticeSetDetailsScreen'
-import PracticeSetEditScreen from './screens/PracticeSetEditScreen'
-import PracticeSetNewProblemScreen from './screens/PracticeSetNewProblemScreen'
-import PracticeProblemEditScreen from './screens/PracticeProblemEditScreen'
-import PracticeSetNewScreen from './screens/PracticeSetNewScreen'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import HomeScreen from './screens/HomeScreen';
+import PracticeHomeScreen from './screens/PracticeHomeScreen';
+import PracticeSetHomeScreen from './screens/PracticeSetHomeScreen';
+import PracticeSetDetailsScreen from './screens/PracticeSetDetailsScreen';
+import PracticeSetEditScreen from './screens/PracticeSetEditScreen';
+import PracticeSetNewProblemScreen from './screens/PracticeSetNewProblemScreen';
+import PracticeProblemEditScreen from './screens/PracticeProblemEditScreen';
+import PracticeSetNewScreen from './screens/PracticeSetNewScreen';
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const App = () => (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Reviso - Home' }} />
-      <Stack.Screen name="PracticeHome" component={PracticeHomeScreen} options={{ title: 'Reviso - Practice Mode' }} />
-      <Stack.Screen name="PracticeSetHome" component={PracticeSetHomeScreen} options={{ title: 'Reviso - Practice Sets' }} />
-      <Stack.Screen name="PracticeSetDetails" component={PracticeSetDetailsScreen} options={({ route }) => ({ title: route.params.title })} />
-      <Stack.Screen name="PracticeSetEdit" component={PracticeSetEditScreen} options={({ route }) => ({ title: route.params.title })} />
-      <Stack.Screen name="PracticeSetNewProblem" component={PracticeSetNewProblemScreen} options={{ title: 'Create New Question' }} />
-      <Stack.Screen name="PracticeProblemEdit" component={PracticeProblemEditScreen} options={{ title: 'Edit Question' }} />
-      <Stack.Screen name="PracticeSetNew" component={PracticeSetNewScreen} options={{ title: 'Create New Problem Set' }} />
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Reviso - Home' }}
+      />
+      <Stack.Screen
+        name="PracticeHome"
+        component={PracticeHomeScreen}
+        options={{ title: 'Reviso - Practice Mode' }}
+      />
+      <Stack.Screen
+        name="PracticeSetHome"
+        component={PracticeSetHomeScreen}
+        options={{ title: 'Reviso - Practice Sets' }}
+      />
+      <Stack.Screen
+        name="PracticeSetDetails"
+        component={PracticeSetDetailsScreen}
+        options={({ route }) => ({ title: route.params.title })}
+      />
+      <Stack.Screen
+        name="PracticeSetEdit"
+        component={PracticeSetEditScreen}
+        options={({ route }) => ({ title: route.params.title })}
+      />
+      <Stack.Screen
+        name="PracticeSetNewProblem"
+        component={PracticeSetNewProblemScreen}
+        options={{ title: 'Create New Question' }}
+      />
+      <Stack.Screen
+        name="PracticeProblemEdit"
+        component={PracticeProblemEditScreen}
+        options={{ title: 'Edit Question' }}
+      />
+      <Stack.Screen
+        name="PracticeSetNew"
+        component={PracticeSetNewScreen}
+        options={{ title: 'Create New Problem Set' }}
+      />
     </Stack.Navigator>
   </NavigationContainer>
-)
+);
 
 export default () => (
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,131 @@
-import React from 'react'
-import { View, Button } from 'react-native'
-import styles from '../styles'
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  TextInput,
+} from 'react-native';
+import styles from './HomeScreenStyles';
 
-export default ({ navigation }) => (
-  <View style={styles.topLevel}>
-    <View style={styles.paddedElement}><Button title="Sign Up" disabled={true} /></View>
-    <View style={styles.paddedElement}><Button title="Log In" disabled={true} /></View>
-    <View style={styles.paddedElement}><Button title="Practice" onPress={() => navigation.navigate('PracticeHome')} /></View>
-  </View>
-)
+export default ({ navigation }) => {
+  return (
+    <ScrollView style={styles.container}>
+      <View>
+        <View
+          style={{
+            marginTop: '5%',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Image
+            style={{ width: '40%', height: '40%' }}
+            source={require('../assets/LogoMakr.png')}
+          />
+          <Text
+            style={[
+              styles.text,
+              {
+                marginTop: '5%',
+                fontSize: 22,
+                fontWeight: '500',
+              },
+            ]}
+          >
+            Reviso
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: '-5%',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}
+        >
+          <TouchableOpacity>
+            <View style={styles.socialButton}>
+              <Image
+                source={require('../assets/facebook.png')}
+                style={styles.socialLogo}
+              />
+              <Text style={styles.text}>Facebook</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialButton}>
+            <Image
+              source={require('../assets/google.png')}
+              style={styles.socialLogo}
+            />
+            <Text style={styles.text}>Google</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text
+          style={[
+            styles.text,
+            {
+              color: '#ABB4BD',
+              fontSize: 15,
+              textAlign: 'center',
+              marginVertical: 20,
+            },
+          ]}
+        >
+          or
+        </Text>
+
+        <View style={styles.inputTitle}>
+          <Text style={styles.inputTitle}>Email</Text>
+          <TextInput style={styles.input} />
+          <View
+            style={{ borderBottomColor: '#D8D8D8', borderBottomWidth: 1 }}
+          />
+        </View>
+        <View style={styles.inputTitle}>
+          <Text style={styles.inputTitle}>Password</Text>
+          <TextInput secureTextEntry={true} style={styles.input} />
+          <View
+            style={{ borderBottomColor: '#D8D8D8', borderBottomWidth: 1 }}
+          />
+        </View>
+
+        <Text style={[styles.text, styles.link, { textAlign: 'right' }]}>
+          Forgot Password?
+        </Text>
+
+        <TouchableOpacity style={styles.submitContainer}>
+          <Text
+            style={[
+              styles.text,
+              {
+                color: '#FFF',
+                fontWeight: '600',
+                fontSize: 16,
+              },
+            ]}
+          >
+            Login
+          </Text>
+        </TouchableOpacity>
+
+        <Text
+          style={[
+            styles.text,
+            {
+              fontSize: 14,
+              color: '#ABB4BD',
+              textAlign: 'center',
+              marginTop: 24,
+            },
+          ]}
+        >
+          Don't have an account?{' '}
+          <Text style={[styles.text, styles.link]}>Register Now</Text>
+        </Text>
+      </View>
+    </ScrollView>
+  );
+};
